8c98281730477fb251bf35734a83d544361f1ca5
luckydut97 <luckydut97@gmail.com>
2025-08-11 18:13:16 +0900

    release_v2.0.8_250808

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.luckydut97.tennispark"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.0.7"
+        versionCode = 18
+        versionName = "2.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
